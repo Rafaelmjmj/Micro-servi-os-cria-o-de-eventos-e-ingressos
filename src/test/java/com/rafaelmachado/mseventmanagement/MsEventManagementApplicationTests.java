b5a4b011e4b630097df454c348f635943b53d735
Rafael Machado <rafaelmachadojeziorny@gmail.com>
2025-03-07 14:57:34 -0300

    feat criando projeto

---
+++ src/test/java/com/rafaelmachado/mseventmanagement/MsEventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmachado.mseventmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
