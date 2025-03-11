1993d951ffbf08f491e9de2c7ac48cc885ea077f
Rafael Machado <rafaelmachadojeziorny@gmail.com>
2025-03-11 16:40:42 -0300

    chore: juntando dois projetos em apenas uma pasta

--- src/main/java/com/rafaelmachado/mseventmanagement/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rafaelmachado.mseventmanagement.repository;
-
-import com.rafaelmachado.mseventmanagement.model.Event;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends MongoRepository<Event, String> {
-}
